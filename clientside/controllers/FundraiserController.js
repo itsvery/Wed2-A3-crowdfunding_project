4280aeec340d7ebc86472fc4294aa376b1561179
Alex Fu <2732379638@qq.com>
2024-10-12 19:09:22 +0800

    Modify api
    Modify not getting database data due to incorrect url of api request.

--- clientside/controllers/FundraiserController.js
+++ clientside/controllers/FundraiserController.js
@@ -1,7 +1,6 @@
 app.controller('FundraiserController', function($scope, $http, $routeParams) {
-    var fundraiserId = $routeParams.id;
-    $http.get('/fundraiser/' + fundraiserId).then(function(response) {
-      $scope.fundraiser = response.data;
-    });
+  var fundraiserId = $routeParams.id;
+  $http.get('http://localhost:3000/fundraiser/' + fundraiserId).then(function(response) {
+    $scope.fundraiser = response.data;
   });
-  
+});
--- clientside/controllers/HomeController.js
+++ clientside/controllers/HomeController.js
@@ -1,10 +1,9 @@
 app.controller('HomeController', function($scope, $http) {
-    $scope.message = "Welcome to Our Crowdfunding Platform!";
-    $scope.inspiringStories = "Here are some inspiring stories...";
-    $scope.contactInfo = "Contact us at: contact@crowdfunding.com";
-  
-    $http.get('/fundraisers').then(function(response) {
-      $scope.fundraisers = response.data;
-    });
+  $scope.message = "Welcome to Our Crowdfunding Platform!";
+  $scope.inspiringStories = "Here are some inspiring stories...";
+  $scope.contactInfo = "Contact us at: contact@crowdfunding.com";
+
+  $http.get('http://localhost:3000/fundraisers').then(function(response) {
+    $scope.fundraisers = response.data;
   });
-  
+});
