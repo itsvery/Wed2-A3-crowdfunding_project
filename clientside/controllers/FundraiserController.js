572a423957ef8693ccbbe37175c47f4fbaa29189
Alex Fu <2732379638@qq.com>
2024-10-12 20:05:35 +0800

    Client-side rewriting
    It is possible that due to an inherent problem with the AngularJS framework, or a network transmission problem, the component is not able to parse the json data properly causing the page to fail to display the information.

--- clientside/controllers/FundraiserController.js
+++
@@ -1,6 +0,0 @@
-app.controller('FundraiserController', function($scope, $http, $routeParams) {
-  var fundraiserId = $routeParams.id;
-  $http.get('http://localhost:3000/fundraiser/' + fundraiserId).then(function(response) {
-    $scope.fundraiser = response.data;
-  });
-});
