65dd1ca3f85f4874d01431e0e1b53fc97684664f
Alex Fu <2732379638@qq.com>
2024-10-13 14:10:20 +0800

    Creating an Angular Project
    Create Angular project and delete AngularJS.

---
+++ clientside/crowdfunding-platform/src/app/fundraiser/fundraiser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fundraiser',
+  standalone: true,
+  imports: [],
+  templateUrl: './fundraiser.component.html',
+  styleUrl: './fundraiser.component.css'
+})
+export class FundraiserComponent {
+
+}
